f550dd5ea989d702e34f33835bb2c978c904a4d1
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-07-21 01:52:14 -0300

    Create implementation for Point2D methods

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,13 +1,14 @@
-@file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
+
+import kotlin.math.*
 
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(p.x + x, p.y + y)
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    return Point2D(v.dx + x, v.dy + y)
   }
 
   override fun toString(): String {
@@ -15,26 +16,26 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(x, y)
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(x - p.x, y - p.y)
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactVector(p).unit
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactVector(p).normal
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return impactDirection(p).normal
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    return sqrt((x - p.x)^2 + (y - p.y)^2)
   }
 }
